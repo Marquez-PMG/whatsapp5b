0e4d6eb57219368fc4ca03cae789b27214db8c6b
Michael_MP <125755142+Marquez-PMG@users.noreply.github.com>
2025-05-06 18:59:03 -0500

    Primer commit

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "apple5a";
+    private $username = "apple5a";
+    private $password = "apple5a";
+    private $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $dsn = "mysql:host={$this->host};dbname={$this->db_name};charset=utf8mb4";
+            $this->conn = new PDO($dsn, $this->username, $this->password, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        } catch (PDOException $exception) {
+            error_log("Connection error: " . $exception->getMessage());
+            throw new Exception("No se pudo conectar a la base de datos.");
+        }
+        return $this->conn;
+    }
+}
+?>
